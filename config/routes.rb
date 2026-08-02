431cf653fe082672ee29474d57508d9e6ab9843e
haaruu06 <c1503847@st.kanazawa-it.ac.jp>
2026-08-02 20:26:13 +0900

    routes.rbの反映

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :tasks, only: [:index, :create, :new, :update]
   get "tasks/completed", to: "tasks#completed", as: :completed_tasks
   root 'tasks#index' # トップページをindexにする場合
+  get "tasks/main", to:"tasks#main"
 end
