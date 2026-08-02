ef2f07a76f6b884736656ae83b9907018c73bcbf
aaaa <gunnma2007@gmial.com>
2026-08-02 19:06:25 +0900

    Resolve merge conflict in migration file

--- db/migrate/20260802061304_add_completed_to_tasks.rb
+++ db/migrate/20260802061304_add_completed_to_tasks.rb
@@ -1,9 +1,5 @@
 class AddCompletedToTasks < ActiveRecord::Migration[8.1]
   def change
-<<<<<<< HEAD
     add_column :tasks, :completed, :boolean, default: false
-=======
-    add_column :tasks, :completed, :boolean
->>>>>>> 5b163aa6e6941f9a781ae5b3d87ca2d72762c5dc
   end
 end
